1a8d7e245a6e119d5bb6f9221b0a9f16ce2f1062
vovan-u-s <oversoul6661@gmail.com>
2025-09-25 20:45:05 -0500

    formated by test before

--- tests/form-authentication-tests.spec.ts
+++ tests/form-authentication-tests.spec.ts
@@ -2,35 +2,32 @@ import { expect, test, Page } from '@playwright/test'
 import { HomePage } from '../page/homepage'
 import { LoggedPage } from '../page/loggedpage'
 import { FormAuthenticationPage } from '../page/formAuthentication'
+let homePage: HomePage
+let formAuthenticationPage: FormAuthenticationPage
+let loggedPage: LoggedPage
+test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page)
+    formAuthenticationPage = new FormAuthenticationPage(page)
+    loggedPage = new LoggedPage(page)
+    await page.goto('https://the-internet.herokuapp.com/')
 
+})
 
 test('test of check form authentication', async ({ page }) => {
-    await page.goto('https://the-internet.herokuapp.com/')
-    let homePage = new HomePage(page)
     await homePage.clickOnFormAuthentication()
-    let formAuthenticationPage = new FormAuthenticationPage(page)
     await formAuthenticationPage.enterUsernameAndPasswordAndPressLogin('tomsmith', 'SuperSecretPassword!')
-    let loggedPage = new LoggedPage(page)
     await loggedPage.verifySuccessMessageIsVisible()
     await loggedPage.clickLogoutButton()
     await formAuthenticationPage.verifyLogoutMessageIsVisible()
-
-
 })
 test('negative login test', async ({ page }) => {
-    await page.goto('https://the-internet.herokuapp.com/')
-    let homePage = new HomePage(page)
     await homePage.clickOnFormAuthentication()
-    let formAuthenticationPage = new FormAuthenticationPage(page)
     await formAuthenticationPage.enterUsernameAndPasswordAndPressLogin('dfhdfhdfhd', 'SuperSecretPassword!')
     await formAuthenticationPage.verifyInvalidUsernameMessageIsVisible()
 })
 test('negative password test', async ({ page }) => {
-    await page.goto('https://the-internet.herokuapp.com/')
-    let homePage = new HomePage(page)
     await homePage.clickOnFormAuthentication()
-    let formAuthenticationPage = new FormAuthenticationPage(page)
     await formAuthenticationPage.enterUsernameAndPasswordAndPressLogin('tomsmith', 'fgdfgdfgd')
     await formAuthenticationPage.verifyInvalidPasswordMessageIsVisible()
-    
+
 })
